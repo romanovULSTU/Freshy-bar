89150a3c3a8281a2ec956e51e94be91ecb036eac
devExploitt <smart.romanova15@gmail.com>
2023-09-06 00:54:32 +0400

    init
    modal make your own and calculate total price

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 const API_URL = "https://hungry-sunrise-mimosa.glitch.me/";
 
+const price = {
+    Клубника: 60,
+    Банан: 50,
+    Манго: 70,
+    Киви: 55,
+    Маракуйя: 90,
+    Яблоко: 45,
+    Мята: 50,
+    Лед: 10,
+    Биоразлагаемый: 20,
+    Пластиковый: 0,
+};
+
 const getData = async () => {
     const response = await fetch(`${API_URL}api/goods`)
     const data = await response.json();
@@ -12,9 +25,11 @@ const createCard = (item) => {
     cocktail.classList.add('cocktail');
 
     cocktail.innerHTML = `
-        <img src="${API_URL}${item.image}"
-        alt="Коктейль ${item.title}" 
-        class="cocktail__img">
+        <img 
+            src="${API_URL}${item.image}"
+            alt="Коктейль ${item.title}" 
+            class="cocktail__img"
+        >
 
         <div class="coctail__content">
             <div class="cocktail__text">
@@ -23,14 +38,36 @@ const createCard = (item) => {
                 <p class="cocktail__size"> ${item.size}</p>
             </div>
 
-            <button class="btn cocktail__btn data-id="${item.id}">Добавить</button>
+            <button class="btn cocktail__btn cocktail__btn_add" data-id="${item.id}">Добавить</button>
         </div>
     `;
     return cocktail;
-}
+};
+
+const scrollService = {
+    scrollPosition: 0,
+    disabledScroll() {
+        this.scrollPosition = window.scrollY;
+        document.documentElement.style.scrollBehavior = "auto";
+        document.body.style.cssText = `
+        overflow: hidden;
+        position: fixed;
+        top: -${this.scrollPosition}px;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+        padding-right: ${window.innerWidth - document.body.offsetWidth}px;
+        `;
+    },
+    enabledScroll() {
+        document.body.style.cssText = "";
+        window.scroll({ top: this.scrollPosition });
+        document.documentElement.style.scrollBehavior = "";
+    },
+};
 
 const modalController = ({ modal, btnOpen, time = 300 }) => {
-    const buttonElem = document.querySelector(btnOpen);
+    const buttonElems = document.querySelectorAll(btnOpen);
     const modalElem = document.querySelector(modal);
 
     modalElem.style.cssText = `
@@ -46,9 +83,10 @@ const modalController = ({ modal, btnOpen, time = 300 }) => {
 
         if (target === modalElem || code === "Escape") {
             modalElem.style.opacity = 0;
-            
+
             setTimeout(() => {
                 modalElem.style.visibility = "hidden";
+                scrollService.enabledScroll();
             }, time);
 
             window.removeEventListener('keydown', closeModal);
@@ -59,19 +97,90 @@ const modalController = ({ modal, btnOpen, time = 300 }) => {
         modalElem.style.visibility = "visible";
         modalElem.style.opacity = 1;
         window.addEventListener('keydown', closeModal);
+        scrollService.disabledScroll();
     };
 
-    buttonElem.addEventListener("click", openModal);
+    buttonElems.forEach(buttonElem => {
+        buttonElem.addEventListener("click", openModal);
+    });
+
     modalElem.addEventListener("click", closeModal);
 
     return { openModal, closeModal };
+};
+
+const getFormData = (form) => {
+    const formData = new FormData(form);
+    const data = {};
+
+    for (const [name, value] of (formData.entries())) {
+        if (data[name]) {
+            if (!Array.isArray(data[name])) {
+                data[name] = [data[name]];
+            }
+            data[name].push(value);
+        } else {
+            data[name] = value;
+        }
+    }
+
+    return data;
+};
+
+const calculateTotalPrice = (form, startPrice) => {
+    let totalPrice = startPrice;
+
+    const data = getFormData(form);
+
+    if (Array.isArray(data.ingredients)){
+        data.ingredients.forEach(item => {
+            totalPrice += price[item] || 0;
+        })
+    } else {
+        totalPrice += price[data.ingredients] || 0;
+    }
+
+    if (Array.isArray(data.topping)){
+        data.topping.forEach(item => {
+            totalPrice += price[item] || 0;
+        })
+    } else {
+        totalPrice += price[data.topping] || 0;
+    }
+
+        totalPrice += price[data.cup] || 0;
+
+        return totalPrice;
+}
+
+const calculateMakeYourOwn = () => {
+    const formMakeOwn = document.querySelector('.make__form_make-your-own');
+    const makeInputPrice = formMakeOwn.querySelector('.make__input_price');
+    const makeTotalPrice = formMakeOwn.querySelector('.make__total-price');
+
+    const handlerChange = () => {
+        const totalPrice = calculateTotalPrice(formMakeOwn, 150);
+        makeInputPrice.value = totalPrice;
+        makeTotalPrice.textContent = `${totalPrice} ₽`;
+    }
+
+    formMakeOwn.addEventListener('change', handlerChange);
+    handlerChange();
 }
 
 const init = async () => {
     modalController({
-        modal: ".modal-order",
+        modal: ".modal_order",
         btnOpen: ".header__btn-order",
     });
+
+    calculateMakeYourOwn();
+
+    modalController({
+        modal: '.modal_make-your-own',
+        btnOpen: '.cocktail__btn_make',
+    });
+
 
     const goodsListElem = document.querySelector(".goods__list");
     const data = await getData();
@@ -80,10 +189,15 @@ const init = async () => {
         const li = document.createElement('li');
         li.classList.add("goods__item");
         li.append(createCard(item))
-        console.log('item: ', item);
+
         return li;
     })
     goodsListElem.append(...cartsCocktail);
+
+    modalController({
+        modal: ".modal_add",
+        btnOpen: ".cocktail__btn_add",
+    })
 };
 
 init();
